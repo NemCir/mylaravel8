9770921a53bdc129887a5d2881e8cfc1c13d4db7
n.cirovic <n.cirovic@logispin.com>
2022-05-23 17:21:06 +0200

    Adding posts

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
@@ -38,6 +39,5 @@ Route::get('/dashboard', [DashboardController::class, 'index'])
     //One way of checking auth with middleware, other way created in controller
     //->middleware('auth');
 
-Route::get('/posts', function () {
-    return view('posts.index');
-});
+Route::get('/posts', [PostController::class, 'index'])->name('posts');
+Route::post('/posts', [PostController::class, 'store']);
